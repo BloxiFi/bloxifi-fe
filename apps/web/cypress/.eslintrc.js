764e980029207d9cbe2f46f603faa0c75778e4b3
aleksaToljic <aleksa.toljic@gmail.com>
2022-06-04 22:12:39 +0200

    chore: Some config improvements

--- apps/web/cypress/.eslintrc.js
+++ apps/web/cypress/.eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: [
-    '../../../.eslintrc.js',
-    'plugin:cypress/recommended',
-  ],
+  extends: ['../../../.eslintrc.js', 'plugin:cypress/recommended'],
   env: {
     node: true,
     browser: true,
