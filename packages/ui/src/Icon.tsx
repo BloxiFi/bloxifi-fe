b966a75aa10ac9e2f21f102c1e83805349f2bfe3
Kristina2103 <kristinapejcic1994@gmail.com>
2022-06-21 12:11:53 +0200

    feat: Icon component [BLOX-170]

---
+++ packages/ui/src/Icon.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import InlineSVG, { Props } from 'react-inlinesvg'
+import styled from 'styled-components'
+
+export const IconNames = [
+  'close',
+  'arrow-down',
+  'arrow-up',
+  'arrow-right',
+  'settings',
+  'total-borrowed',
+  'total-deposited',
+  'union',
+] as const
+type IconNamesType = typeof IconNames[number]
+
+export interface IconProps extends Omit<Props, 'src'> {
+  /**
+   * Possible values for `name` prop of `Icon` component.
+   */
+  readonly name: IconNamesType
+  /**
+   * This number represents width and height of icon. Defaults to 16px
+   * */
+  readonly size?: number
+  /**
+   * This represents icon color you want to render
+   * */
+  readonly color?: string
+}
+
+export const Icon = styled(
+  ({ name, size = 16, color, className, ...props }: IconProps): JSX.Element => {
+    const icon = require(`./icons/${name}.svg`) as string
+
+    return (
+      <InlineSVG
+        src={icon}
+        //TODO LOADER COMPONENT loader={<Loader loaderSize={size} />}
+        style={{ width: size, height: size }}
+        className={`rts-icon ${className}`}
+        fill={color && color}
+        {...props}
+      />
+    )
+  },
+)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
---
+++ packages/ui/src/stories/Icon.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import { Icon, IconNames, IconProps } from '../Icon'
+import styled from 'styled-components'
+import { Colors } from '../styles/colors'
+
+export default {
+  title: 'Components/Icon',
+  component: Icon,
+  args: { name: 'arrow-down', size: 36, color: Colors.textGray },
+} as Meta<IconProps>
+
+export const AllIcons = () => {
+  return (
+    <Wrapper>
+      {IconNames.map(iconName => (
+        <div key={iconName}>
+          <Icon size={36} name={iconName} />
+          <div>{iconName}</div>
+        </div>
+      ))}
+    </Wrapper>
+  )
+}
+
+export const Details: Story<IconProps> = args => {
+  return <Icon {...args} />
+}
+
+Details.storyName = 'Details'
+
+AllIcons.storyName = 'All icons'
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: max-content;
+  grid-gap: 1rem;
+
+  svg {
+    margin: auto;
+  }
+`
