b0b3a0b2ec328a0ac9f8852df74e3e900582d8e1
Kristina2103 <kristinapejcic1994@gmail.com>
2022-06-22 13:32:51 +0200

    feat: Use light theme colors inside Storybook layout [SBA-166]

--- packages/ui/src/styles/colors.ts
+++ packages/ui/src/styles/colors.ts
@@ -1,28 +1,6 @@
 export const Colors = {
-  headerBackground: '#F6F8FA',
-  logoBackground: '#57606A',
-  background: '#FFFFFF',
-
-  //Borders
-  borderColor: '#D0D7DE',
-  borderLight: 'rgba(0,0,0,0.2)',
-  borderDark: 'rgb(0,0,0)',
-
-  textColorLight: '#818181',
-  textColorDark: '#212121',
-  primary: '#212529',
-
-  purple70: '#5B1F7B',
-
   gray30: '#EFF0F6',
-  gray60: '#A6A9C3',
-  gray70: '#979AB8',
-  gray80: '#6E7191',
-  gray90: '#4E4B66',
-
-  white: '#fff',
-
-  chartTitleBackground: '#F7F9FC',
+  borderLight: 'rgba(0,0,0,0.2)',
 
   light: {
     headerBackground: '#F6F8FA',
@@ -30,6 +8,7 @@ export const Colors = {
     background: '#FFFFFF',
     tabHover: '#218BFF',
     white: '#FFFFFF',
+    chartTitleBackground: '#F7F9FC',
 
     //Text
     textColorLight: '#818181',
@@ -38,7 +17,16 @@ export const Colors = {
 
     red60: '#E64073',
     red80: '#CF295C',
+
     green80: '#098469',
+
+    purple70: '#5B1F7B',
+
+    gray30: '#EFF0F6',
+    gray60: '#A6A9C3',
+    gray70: '#979AB8',
+    gray80: '#6E7191',
+    gray90: '#4E4B66',
 
     //Notifications
     warningNotification: '#ffb058',
@@ -65,6 +53,7 @@ export const Colors = {
     background: '#FFFFFF',
     tabHover: '#218BFF',
     white: '#000000',
+    chartTitleBackground: '#F7F9FC',
 
     //Text
     textColorLight: '#818181',
@@ -73,7 +62,16 @@ export const Colors = {
 
     red60: '#E64073',
     red80: '#CF295C',
+
     green80: '#098469',
+
+    purple70: '#5B1F7B',
+
+    gray30: '#EFF0F6',
+    gray60: '#A6A9C3',
+    gray70: '#979AB8',
+    gray80: '#6E7191',
+    gray90: '#4E4B66',
 
     //Notifications
     warningNotification: '#ffb058',
