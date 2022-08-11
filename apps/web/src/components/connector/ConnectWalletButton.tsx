22cbb673ede01e740833caf63d9ed4afb7a8c6a7
Kristina <kristinapejcic1994@gmail.com>
2022-08-11 17:04:44 +0200

    feat: Remove Settings button from Header for MVP [BLOX-215]

--- apps/web/src/components/connector/ConnectWalletButton.tsx
+++ apps/web/src/components/connector/ConnectWalletButton.tsx
@@ -9,7 +9,6 @@ import {
   Icon,
   Text,
   StackLayout,
-  Toggle,
 } from '@bloxifi/ui'
 import { sliceMiddleOfString } from '@bloxifi/core'
 import { useTranslation } from 'react-i18next'
@@ -20,8 +19,6 @@ export const ConnectWalletButton = () => {
   const { t } = useTranslation()
   const MOONBASE_EXPLORER = process.env.MOONBASE_EXPLORER
   const [closeMenu, setCloseMenu] = useState(false)
-  const [darkMode, toggleDarkMode] = useState(false)
-  const [testnetMode, toggleTestnetMode] = useState(false)
 
   const {
     connectWallet,
@@ -60,57 +57,6 @@ export const ConnectWalletButton = () => {
     )
   }
 
-  const settings = (
-    <Menu
-      right
-      bottom
-      positionOffset={{ top: 5, left: 0 }}
-      forceClose={closeMenu}
-      onClose={handleClose}
-      toggler={
-        <Button
-          appearance="primary-ghost"
-          variant="medium"
-          size="medium"
-          icon="settings"
-        />
-      }
-      field={
-        <StackLayout>
-          <MenuItemTitle type="heading 3">
-            {t('header.globalSettings')}
-          </MenuItemTitle>
-
-          <MenuItem
-            appearance="text"
-            variant="large"
-            size="large"
-            onClick={() => toggleDarkMode(!darkMode)}
-          >
-            {t('global.buttons.darkMode')}{' '}
-            <Toggle
-              checked={darkMode}
-              styleProps={{ width: 50, paddingTop: 2 }}
-            />
-          </MenuItem>
-          <MenuItem
-            appearance="text"
-            variant="large"
-            size="large"
-            isLastItem
-            onClick={() => toggleTestnetMode(!testnetMode)}
-          >
-            {t('global.buttons.testnetMode')}{' '}
-            <Toggle
-              checked={testnetMode}
-              styleProps={{ width: 50, paddingTop: 2 }}
-            />
-          </MenuItem>
-        </StackLayout>
-      }
-    />
-  )
-
   return isConnected ? (
     <ColumnLayout>
       {/**TODO handle BLOX balance button click when we get BLOX token on Moonbeam, wait for BE to generate it */}
@@ -118,56 +64,50 @@ export const ConnectWalletButton = () => {
         {t('global.buttons.bloxBalance')}
       </Button>
       {isSupportedNetwork ? (
-        <ColumnLayout>
-          <Menu
-            left
-            bottom
-            positionOffset={{ top: 5, left: 0 }}
-            forceClose={closeMenu}
-            onClose={handleClose}
-            toggler={
-              <Button appearance="primary-ghost" variant="medium" size="medium">
-                <>
-                  {sliceMiddleOfString(currentAccount, 4)}
-                  <BoxLayout gap={0.75}>
-                    <Icon name="arrow-down" color="white" />
-                  </BoxLayout>
-                </>
-              </Button>
-            }
-            field={
-              <StackLayout>
-                <MenuItemTitle type="heading 3">
-                  {sliceMiddleOfString(currentAccount, 4)}
-                </MenuItemTitle>
+        <Menu
+          right
+          bottom
+          positionOffset={{ top: 5, left: 0 }}
+          forceClose={closeMenu}
+          onClose={handleClose}
+          toggler={
+            <Button appearance="primary-ghost" variant="medium" size="medium">
+              <>
+                {sliceMiddleOfString(currentAccount, 4)}
+                <BoxLayout gap={0.75}>
+                  <Icon name="arrow-down" color="white" />
+                </BoxLayout>
+              </>
+            </Button>
+          }
+          field={
+            <StackLayout>
+              <MenuItemTitle type="heading 3">
+                {sliceMiddleOfString(currentAccount, 4)}
+              </MenuItemTitle>
 
-                <MenuItem
-                  appearance="text"
-                  variant="large"
-                  size="large"
-                  onClick={() =>
-                    window.open(
-                      `${MOONBASE_EXPLORER}${currentAccount}`,
-                      '_blank',
-                    )
-                  }
-                >
-                  {t('global.buttons.viewOnExplorer')}
-                </MenuItem>
-                <MenuItem
-                  appearance="text"
-                  variant="large"
-                  size="large"
-                  onClick={handleDisconnect}
-                  isLastItem
-                >
-                  {t('global.buttons.disconnectWallet')}
-                </MenuItem>
-              </StackLayout>
-            }
-          />
-          {settings}
-        </ColumnLayout>
+              <MenuItem
+                appearance="text"
+                variant="large"
+                size="large"
+                onClick={() =>
+                  window.open(`${MOONBASE_EXPLORER}${currentAccount}`, '_blank')
+                }
+              >
+                {t('global.buttons.viewOnExplorer')}
+              </MenuItem>
+              <MenuItem
+                appearance="text"
+                variant="large"
+                size="large"
+                onClick={handleDisconnect}
+                isLastItem
+              >
+                {t('global.buttons.disconnectWallet')}
+              </MenuItem>
+            </StackLayout>
+          }
+        />
       ) : (
         <Button appearance="text" variant="medium" size="medium" color="red">
           {t('walletConnection.wrongNetworkConnection')}
@@ -175,16 +115,13 @@ export const ConnectWalletButton = () => {
       )}
     </ColumnLayout>
   ) : (
-    <ColumnLayout>
-      <Button
-        onClick={connectWallet}
-        variant="medium"
-        appearance="primary"
-        size="medium"
-      >
-        {t('global.buttons.connectWallet')}
-      </Button>
-      {settings}
-    </ColumnLayout>
+    <Button
+      onClick={connectWallet}
+      variant="medium"
+      appearance="primary"
+      size="medium"
+    >
+      {t('global.buttons.connectWallet')}
+    </Button>
   )
 }
