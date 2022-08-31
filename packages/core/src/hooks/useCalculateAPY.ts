b4e4ec65d1f19d2b81f4f3713482e491126b9491
vculum <106971971+vculum-bloxico@users.noreply.github.com>
2022-08-31 17:11:36 +0200

    feat: Create APY hook-3 [BLOX-221]
    feat: Create APY hook-3 [BLOX-221]

--- packages/core/src/hooks/useCalculateAPY.ts
+++ packages/core/src/hooks/useCalculateAPY.ts
@@ -62,7 +62,7 @@ export const useCalculateAPY = ({
       const secondsAmount = daysAmount * SECOND_PER_DAY
       //Daily yield = The number of total tokens staked × (APY for the staked token ÷ 365)
       //const returnValue = (daysAmount * (tokenAmount * (APY / 365))) + tokenAmount
-      const returnValue = tokenAmount + ((APY) * tokenAmount) //APY/secondsAmount ?
+      const returnValue = tokenAmount + APY * tokenAmount //APY/secondsAmount ?
       setReturnAmount(returnValue)
       console.log('APR:', APR, 'APY: ', APY, 'return: ', returnValue)
     })
