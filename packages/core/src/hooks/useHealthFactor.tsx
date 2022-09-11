9195c9cde8e71d0dba650722dba2884c3912d033
Kristina <kristinapejcic1994@gmail.com>
2022-09-11 16:43:00 +0200

    feat: Remove HF refetch [BLOX-229]

--- packages/core/src/hooks/useHealthFactor.tsx
+++ packages/core/src/hooks/useHealthFactor.tsx
@@ -13,14 +13,13 @@ import {
 /**
  * Calculate total borrows for current user
  */
-const calculateTotalBorrow = (array: HealthFactorQuery[]): number => {
-  return array.reduce(function (acc, { currentTotalDebt, reserve: { price } }) {
+const calculateTotalBorrow = (array: HealthFactorQuery[]): number =>
+  array.reduce(function (acc, { currentTotalDebt, reserve: { price } }) {
     return (
       acc +
       bigNumberToNumber(currentTotalDebt) * bigNumberToNumber(price.priceInEth)
     )
   }, 0)
-}
 
 /**
  * Calculate total deposited balance for collaterals with asset liquidation threshold
@@ -70,56 +69,37 @@ export interface HealthFactorData {
   totalBorrowETH: number
 }
 
-export interface HealthFactorReturnValue extends HealthFactorData {
-  /**
-   * Method that returns refetched health factor data
-   */
-  refetchHF: () => void
-  ready: boolean
-}
 /**
  * Hook that returns calculated Health Factor value
  */
 export const useHealthFactor = ({
   currentAccount,
-}: Props = {}): HealthFactorReturnValue => {
+}: Props = {}): HealthFactorData => {
   const [value, setValue] = useState({} as HealthFactorData)
 
-  const { data, refetch, called, loading } = useQuery<
-    HealthFactorGraph,
-    UserReserveVariables
-  >(GET_HEALTH_FACTOR_DATA, {
-    variables: {
-      user: currentAccount?.toLowerCase(),
+  const { data } = useQuery<HealthFactorGraph, UserReserveVariables>(
+    GET_HEALTH_FACTOR_DATA,
+    {
+      variables: {
+        user: currentAccount?.toLowerCase(),
+      },
+      fetchPolicy: 'cache-and-network',
+      onCompleted: () => {
+        const totalBorrowETH = calculateTotalBorrow(data.userReserves)
+        const totalCollateralETH = calculateTotalCollateralWithLT(
+          data.userReserves,
+        )
+        setValue({
+          healthFactor: calculateHealthFactor({
+            totalCollateralETH,
+            totalBorrowETH,
+          }),
+          totalCollateralETH,
+          totalBorrowETH,
+        })
+      },
     },
-    fetchPolicy: 'cache-and-network',
-    onCompleted: data => setData(data.userReserves),
-  })
-  const refetchData = async () => {
-    try {
-      const res: { data: HealthFactorGraph } = await refetch()
-      setData(res.data.userReserves)
-    } catch (error) {
-      //@TODOhandle error
-    }
-  }
+  )
 
-  const setData = (userReserves: HealthFactorQuery[]) => {
-    const totalBorrowETH = calculateTotalBorrow(userReserves)
-    const totalCollateralETH = calculateTotalCollateralWithLT(userReserves)
-    setValue({
-      healthFactor: calculateHealthFactor({
-        totalCollateralETH,
-        totalBorrowETH,
-      }),
-      totalCollateralETH,
-      totalBorrowETH,
-    })
-  }
-
-  return {
-    ...value,
-    refetchHF: refetchData,
-    ready: called && !loading && !!data,
-  }
+  return value
 }
