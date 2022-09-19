cebccd222d59964f3d122eb845ef42a0bd8c5cab
Kristina <kristinapejcic1994@gmail.com>
2022-09-19 14:32:55 +0200

    fix: Change repay and deposit success message [BLOX-229]

--- apps/web/src/components/borrow/modal/RepayModal.tsx
+++ apps/web/src/components/borrow/modal/RepayModal.tsx
@@ -159,7 +159,7 @@ export const RepayModal = ({
           loading={loading}
           error={hasError}
           success={repayCompleted}
-          messages={{ success: t('global.notifications.withdraw_successful') }}
+          messages={{ success: t('global.notifications.repay_successful') }}
         />
       ) : (
         <>
