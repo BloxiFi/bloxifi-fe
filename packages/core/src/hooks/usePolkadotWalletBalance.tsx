acb0b76f0b2a0ac915ce7a830d5df30b7556fffe
Kristina <kristinapejcic1994@gmail.com>
2022-11-15 09:53:57 +0100

    fix: Refactor usePolkadotWalletBalance hook [BLOX-328]

--- packages/core/src/hooks/usePolkadotWalletBalance.tsx
+++ packages/core/src/hooks/usePolkadotWalletBalance.tsx
@@ -1,56 +1,73 @@
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import '@polkadot/api-augment'
+import { useCallback, useEffect, useState } from 'react'
 
 /**
  * Options that can be used to configure useWalletBalance() hook.
  */
-export interface Props {
+export interface PolkadotWalletBalanceProps {
   /**
-   * User wallet account
+   * User Polkadot wallet account
    */
   readonly currentAccount?: string
   /**
-   * Connected network ID
+   * Token symbol
    */
-  readonly currentSymbol?: string
+  readonly currentSymbol?: 'KSM' | 'KAR' | 'MOVR' | 'AUSD'
 }
 
 /**
  * Return type of useWalletBalance hook
  */
-export interface TokenBalanceData {
+export interface PolkadotWalletBalance {
   /**
    * Token Free Balance
    */
-  tokenFreeBalance: number
+  tokenFreeBalance: string
+  /**
+   * Loading data state
+   */
+  isLoading: boolean
+  /**
+   * Error
+   */
+  error?: Error
 }
 
-const getKusamaBalance = async (acc: string) => {
-  const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io')
-  const api = await ApiPromise.create({ provider: wsProvider })
-  const { data: balance } = await api.query.system.account(acc)
-  return balance.free.toHuman()
-}
+const tokenConfig = {
+  KSM: {
+    getPolkadotBalance: async (acc: string) => {
+      const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io')
+      const api = await ApiPromise.create({ provider: wsProvider })
+      const { data: balance } = await api.query.system.account(acc)
+      return balance.free.toHuman()
+    },
+  },
+  KAR: {
+    getPolkadotBalance: async (acc: string) => {
+      const wsProvider = new WsProvider('wss://karura-rpc.dwellir.com')
+      const api = await ApiPromise.create({ provider: wsProvider })
+      const { data: balance } = await api.query.system.account(acc)
+      return balance.free.toHuman()
+    },
+  },
 
-const getKaruraBalance = async (acc: string) => {
-  const wsProvider = new WsProvider('wss://karura-rpc.dwellir.com')
-  const api = await ApiPromise.create({ provider: wsProvider })
-  const { data: balance } = await api.query.system.account(acc)
-  return balance.free.toHuman()
-}
-
-const getMovrBalance = async (acc: string) => {
-  const wsProvider = new WsProvider('wss://karura-rpc.dwellir.com')
-  const api = await ApiPromise.create({ provider: wsProvider })
-  const MOVR = await api.query.tokens.accounts(acc, { ForeignAsset: 3 })
-  return MOVR['free'].toHuman()
-}
-
-const getAUSDBalance = async (acc: string) => {
-  const wsProvider = new WsProvider('wss://karura-rpc.dwellir.com')
-  const api = await ApiPromise.create({ provider: wsProvider })
-  const AUSD = await api.query.tokens.accounts(acc, { Token: 'AUSD' })
-  return AUSD['free'].toHuman()
+  MOVR: {
+    getPolkadotBalance: async (acc: string) => {
+      const wsProvider = new WsProvider('wss://karura-rpc.dwellir.com')
+      const api = await ApiPromise.create({ provider: wsProvider })
+      const MOVR = await api.query.tokens.accounts(acc, { ForeignAsset: 3 })
+      return MOVR['free'].toHuman()
+    },
+  },
+  AUSD: {
+    getPolkadotBalance: async (acc: string) => {
+      const wsProvider = new WsProvider('wss://karura-rpc.dwellir.com')
+      const api = await ApiPromise.create({ provider: wsProvider })
+      const AUSD = await api.query.tokens.accounts(acc, { Token: 'AUSD' })
+      return AUSD['free'].toHuman()
+    },
+  },
 }
 
 /**
@@ -59,21 +76,27 @@ const getAUSDBalance = async (acc: string) => {
 export const usePolkadotWalletBalance = ({
   currentAccount,
   currentSymbol,
-}: Props = {}): TokenBalanceData[] => {
-  const balanceReturned: TokenBalanceData[] = []
+}: PolkadotWalletBalanceProps = {}): PolkadotWalletBalance => {
+  const [tokenFreeBalance, setTokenFreeBalance] = useState<string>()
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(undefined)
 
-  if (currentSymbol == 'KSM') {
-    const tb = async () => await getKusamaBalance(currentAccount)
-    const balanceReturned = tb()
-  } else if (currentSymbol == 'KAR') {
-    const tb = async () => await getKaruraBalance(currentAccount)
-    const balanceReturned = tb()
-  } else if (currentSymbol == 'MOVR') {
-    const tb = async () => await getMovrBalance(currentAccount)
-    const balanceReturned = tb()
-  } else if (currentSymbol == 'AUSD') {
-    const tb = async () => await getAUSDBalance(currentAccount)
-    const balanceReturned = tb()
-  }
-  return balanceReturned
+  const fetchBalances = useCallback(async (currentAccount, currentSymbol) => {
+    try {
+      setIsLoading(true)
+      setTokenFreeBalance(
+        await tokenConfig[currentSymbol].getPolkadotBalance(currentAccount),
+      )
+    } catch (error) {
+      setError(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    void fetchBalances(currentAccount, currentSymbol)
+  }, [currentAccount, currentSymbol, fetchBalances])
+
+  return { isLoading, tokenFreeBalance, error }
 }
