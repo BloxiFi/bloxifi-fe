ffdd4dcd7fea09a0fb11c10c8c06cfdee6035862
Kristina <kristinapejcic1994@gmail.com>
2022-12-13 17:01:37 +0100

    feat: Extract configAsstes in separate file [BLOX-374]

--- packages/core/src/utilities/config/index.tsx
+++ packages/core/src/utilities/config/index.tsx
@@ -1,4 +1,5 @@
 import { ethers } from 'ethers'
+import { configAssets, moonbaseAlphaConfigAssets } from './configAssets'
 
 export const MIN_VALUE_FOR_TRANSACTION = 0.0000001
 export const MIN_HEALTH_FACTOR_VALUE = 1.01
@@ -23,68 +24,6 @@ export const AVAILABLE_BORROW_DEVIATION = '0.01'
 //Allow positive fractional number, included zero.(e.g. 0, 1, 0.0, 0.1, 1.0, 99999.000001, 5.10 )
 export const PATTERN_NUMBERS_ONLY = /^[0-9]*\.?[0-9]*$/
 
-export const configAssets = {
-  KSMmb: {
-    underlyingAsset: '0x16db4d26b6bab2a3d17d05f411d0170abb3374ab',
-    fullName: 'Kusama',
-    icon: 'ksm',
-    decimals: 18,
-    aToken: {
-      id: '0xf160bd6a83516c70e9486c5a541fb684cbada671',
-      underlyingAssetDecimals: 18,
-    },
-  },
-  WBTCmb: {
-    underlyingAsset: '0x391aa0bea0b1f3b3f82263b3c4ab1a0dcd58fb63',
-    fullName: 'Wrapped Bitcoin',
-    icon: 'wbtc',
-    decimals: 18,
-    aToken: {
-      id: '0x6f0560c011df8e9ee7f58ab537315bdfb5e14fb5',
-      underlyingAssetDecimals: 18,
-    },
-  },
-  DAImb: {
-    underlyingAsset: '0x425c0a54430b28f831a810fbc91fc0d33a45d67b',
-    fullName: 'DAI',
-    icon: 'dai',
-    decimals: 18,
-    aToken: {
-      id: '0xdfc17928f7030ff6834b4e38b505a542b5dcc5b6',
-      underlyingAssetDecimals: 18,
-    },
-  },
-  WETHmb: {
-    underlyingAsset: '0x632a2b6128319c65461a84907dfaaa72a38590ab',
-    fullName: 'Wrapped Ethereum',
-    icon: 'weth',
-    decimals: 18,
-    aToken: {
-      id: '0xb777da4e5534ac384e091f9f852c39527cdff117',
-      underlyingAssetDecimals: 18,
-    },
-  },
-  USDCmb: {
-    underlyingAsset: '0xa60669f4e3378af9bdde03fb78f2c08fd92a4cdb',
-    fullName: 'USD Coin',
-    icon: 'usdc',
-    decimals: 18,
-    aToken: {
-      id: '0x6965942c28da13ed9979570262de7faa5c336f5b',
-      underlyingAssetDecimals: 18,
-    },
-  },
-  MOWRmb: {
-    underlyingAsset: '0xbb8ade4e87048549be8a3a856d3c55e58f610197',
-    fullName: 'Moonriver',
-    icon: 'mowr',
-    decimals: 18,
-    aToken: {
-      id: '0x14a8552e6af12d6cd8e86012ee47641055dcb65a',
-      underlyingAssetDecimals: 18,
-    },
-  },
-}
 //@TODO UPLOAD & UPDATE ICONS
 export const NetworkRegistry = [
   {
@@ -185,7 +124,7 @@ export const NetworkRegistry = [
     decimals: [18],
     icon: 'mowr',
     isTestnet: true,
-    configAssets,
+    configAssets: moonbaseAlphaConfigAssets,
   },
 ] as const
 
